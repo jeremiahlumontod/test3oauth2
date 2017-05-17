a451cec5f92a248fff0cfb7def7ddad95d7aa2db
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-05-17 23:26:43 +0400

    updated readme instructions on how to use with Postman for testing

--- src/main/java/org/jml/myeis/config/ResourceServerConfig.java
+++ src/main/java/org/jml/myeis/config/ResourceServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
  *The @EnableResourceServer annotation adds a filter of type OAuth2AuthenticationProcessingFilter automatically
@@ -13,6 +14,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    //should match with resource id in oauth_client_details table of oauth2 schema
+    public static final String RESOURCE_ID = "rest_api";
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -20,5 +24,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers("/private/**").authenticated();
     }
 
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(RESOURCE_ID);
+    }
 
 }
