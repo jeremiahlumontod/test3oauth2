4ae514eee46732f5515ccf7e16b2db0e6399e1fe
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-05-17 17:31:39 +0400

    added readme. added refresh_token

--- src/main/java/org/jml/myeis/config/AuthorizationServerConfig.java
+++ src/main/java/org/jml/myeis/config/AuthorizationServerConfig.java
@@ -52,7 +52,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().withClient("my-trusted-client")
-                .authorizedGrantTypes("client_credentials", "password")
+                .authorizedGrantTypes("client_credentials", "password","refresh_token")
                 .authorities("ROLE_CLIENT","ROLE_TRUSTED_CLIENT")
                 .scopes("read","write","trust")
                 .resourceIds("oauth2-resource").accessTokenValiditySeconds(5000)
